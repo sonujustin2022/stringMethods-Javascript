8ca4b96a5cc575a27f38bc97bb1b020db3b39cac
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-26 08:11:19 +0530

    stringValue printed completed

---
+++ printStringValue.js
@@ -0,0 +1,14 @@
+let word = "incredible";
+//print first letter
+
+console.log(word[0]);
+
+//print last letter 
+
+console.log(word[word.length-2]);
+
+//print all the letters
+
+for(let letter of word){
+    console.log(letter);
+}
--- stringMethod.js
+++
@@ -1,7 +0,0 @@
-let word = "malayalam";
-//print first letter
-
-//print last letter 
-
-
-//print all the letters
